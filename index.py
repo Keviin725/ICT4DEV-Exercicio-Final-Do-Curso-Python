d565c3f990b258014f12dfe7bf3cae03469eb10d
Kevin Machava <kevinmachava20@gmail.com>
2023-07-29 10:05:06 +0200

    drawing the board

---
+++ index.py
@@ -0,0 +1,32 @@
+def draw_board():
+    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    print(" 1 2 3")
+    print("A", end=" ")
+    for i in range(3):
+        if board[0][i] == 0:
+            print("__", end=" ")
+        elif board[0][i] == 1:
+            print("X ", end=" ")
+        else:
+            print("O ", end=" ")
+    print()
+    print("B", end=" ")
+    for i in range(3):
+        if board[1][i] == 0:
+            print("__", end=" ")
+        elif board[1][i] == 1:
+            print("X ", end=" ")
+        else:
+            print("O ", end=" ")
+    print()
+    print("C", end=" ")
+    for i in range(3):
+        if board[2][i] == 0:
+            print("__", end=" ")
+        elif board[2][i] == 1:
+            print("X ", end=" ")
+        else:
+            print("O ", end=" ")
+    print()
+
+draw_board()
